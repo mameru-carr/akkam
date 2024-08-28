9700a23a0e46a2adeb7d9a18a0f4374268a2832f
mameru-carr <samuelcarr.official@gmail.com>
2024-08-28 09:30:15 +0530

    Setup vuejs, sass and pug

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,11 @@
+<script setup>
+</script>
+
+<template lang="pug">
+header
+    h1 J Christy Femila
+    p Professor of Religion, Tamil Nadu Theological Seminary
+</template>
+
+<style scoped lang="sass">
+</style>
